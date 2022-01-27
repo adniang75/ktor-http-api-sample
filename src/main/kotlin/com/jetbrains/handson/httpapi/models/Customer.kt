04fa36a59c6ac3fe0763c6666a977a391582acf2
Alassane NIANG <adniang75@gmail.com>
2022-01-27 18:54:14 +0100

    Defining Order model and endpoints

--- src/main/kotlin/com/jetbrains/handson/httpapi/models/Customer.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/models/Customer.kt
@@ -5,4 +5,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
 
-val customerStorage = mutableListOf<Customer>()
+val customerStorage = mutableListOf(
+    Customer("1", "John", "Doe", "johndoe@example.com"),
+    Customer("2", "Jane", "Doe", "janedoe@example.com"),
+    Customer("3", "Elon", "Musk", "elonmusk@example.com"),
+    Customer("4", "Jeff", "Bezos", "jeffbezos@example.com"),
+    Customer("5", "Barack", "Obama", "barackobama@example.com")
+)
+
